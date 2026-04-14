8197e8b3eeddf96a1becb54ea20184daec45cf49
FarhanR675 <farhanrahman675@gmail.com>
2026-04-14 12:50:08 +0100

    Inital spring boot setup

---
+++ fx-triangulation-engine/src/test/java/com/farhan/quant/fx_triangulation_engine/FxTriangulationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhan.quant.fx_triangulation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxTriangulationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
