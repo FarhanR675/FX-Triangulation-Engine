61a877433bb25c0170c9fd3bd21a128b5e333d88
FarhanR675 <farhanrahman675@gmail.com>
2026-04-15 12:27:07 +0100

    Apply spread created in the Fixed Spread Calculator class

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/spread/FixedSpreadCalculator.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/spread/FixedSpreadCalculator.java
@@ -1,5 +1,14 @@
 package com.farhan.quant.fx_triangulation_engine.pricing.spread;
 
+import com.farhan.quant.fx_triangulation_engine.domain.Price;
+
 public class FixedSpreadCalculator implements SpreadCalculator {
 
+    public Price applySpread(double mid, double spread) {
+
+        double bid = mid - spread / 2;
+        double ask = mid + spread / 2;
+
+        return new Price(mid, bid, ask);
+    }
 }
