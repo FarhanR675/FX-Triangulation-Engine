a9a7b55385ec4b5d49c20c2d7d99d3512b512ff5
FarhanR675 <farhanrahman675@gmail.com>
2026-04-15 10:47:33 +0100

    Tick class getters and constructor implemented for streaming/api

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/domain/Tick.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/domain/Tick.java
@@ -1,4 +1,26 @@
 package com.farhan.quant.fx_triangulation_engine.domain;
 
+import java.time.Instant;
+
 public class Tick {
+
+    private final CurrencyPair currencyPair;
+    private final Price price;
+    private final Instant timestamp;
+
+    public Tick(CurrencyPair currencyPair, Price price, Instant timestamp) {
+        this.currencyPair = currencyPair;
+        this.price = price;
+        this.timestamp = timestamp;
+    }
+
+    public CurrencyPair getCurrencyPair() {
+        return currencyPair;
+    }
+    public Price getPrice() {
+        return price;
+    }
+    public Instant getTimestamp() {
+        return timestamp;
+    }
 }
