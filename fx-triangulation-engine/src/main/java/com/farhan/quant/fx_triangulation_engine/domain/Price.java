da57ba3d58054e9744af9a46b60caeb8aa69eb4d
FarhanR675 <farhanrahman675@gmail.com>
2026-04-15 10:31:59 +0100

    Price class created with getters and constructors

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/domain/Price.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/domain/Price.java
@@ -1,4 +1,33 @@
 package com.farhan.quant.fx_triangulation_engine.domain;
 
 public class Price {
+
+    private final double mid;
+    private final double bid;
+    private final double ask;
+
+    public Price(double mid, double bid, double ask) {
+        this.mid = mid;
+        this.bid = bid;
+        this.ask = ask;
+    }
+
+    public double getMid() {
+        return mid;
+    }
+    public double getBid() {
+        return bid;
+    }
+    public double getAsk() {
+        return ask;
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "mid=" + mid +
+                ", bid=" + bid +
+                ", ask=" + ask +
+                '}';
+    }
 }
