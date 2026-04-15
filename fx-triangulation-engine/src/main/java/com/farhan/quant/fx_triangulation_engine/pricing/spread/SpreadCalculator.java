7df7c157aef14d1b768e2aaf7d5f89dc7c6c76bb
FarhanR675 <farhanrahman675@gmail.com>
2026-04-15 12:13:40 +0100

    Spread calculator interface created

---
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/spread/SpreadCalculator.java
@@ -0,0 +1,8 @@
+package com.farhan.quant.fx_triangulation_engine.pricing.spread;
+
+import com.farhan.quant.fx_triangulation_engine.domain.Price;
+
+public interface SpreadCalculator {
+
+    Price applySpread(double midPrice, double spread);
+}
