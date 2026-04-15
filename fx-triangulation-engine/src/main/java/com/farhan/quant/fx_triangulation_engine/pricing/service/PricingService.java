096319339a18c69d9726700e99c37c20e7b862ad
FarhanR675 <farhanrahman675@gmail.com>
2026-04-15 13:15:15 +0100

    Pricing Service logic implemented and connected to tools

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingService.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingService.java
@@ -1,4 +1,25 @@
 package com.farhan.quant.fx_triangulation_engine.pricing.service;
 
+import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
+import com.farhan.quant.fx_triangulation_engine.domain.Price;
+import com.farhan.quant.fx_triangulation_engine.pricing.alpha.AlphaPriceGenerator;
+import com.farhan.quant.fx_triangulation_engine.pricing.spread.SpreadCalculator;
+
 public class PricingService {
+
+    private final AlphaPriceGenerator alphaPriceGenerator;
+    private final SpreadCalculator spreadCalculator;
+
+    public PricingService(AlphaPriceGenerator alphaPriceGenerator, SpreadCalculator spreadCalculator) {
+        this.alphaPriceGenerator = alphaPriceGenerator;
+        this.spreadCalculator = spreadCalculator;
+    }
+
+    public Price getPrice(CurrencyPair currencyPair, double spread) {
+        // Get fair price using mid
+        double mid = alphaPriceGenerator.generateMidPrice(currencyPair);
+
+        // Application of spread (bid/ask)
+        return spreadCalculator.applySpread(mid, spread);
+    }
 }
