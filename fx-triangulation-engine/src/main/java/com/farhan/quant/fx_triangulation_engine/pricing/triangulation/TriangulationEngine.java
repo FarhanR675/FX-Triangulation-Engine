ba7baa1f6436ec85e73972ba79f45bc7f597a474
FarhanR675 <farhanrahman675@gmail.com>
2026-04-16 11:54:08 +0100

    Computer Cross Rate Implemented

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/triangulation/TriangulationEngine.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/triangulation/TriangulationEngine.java
@@ -2,4 +2,7 @@ package com.farhan.quant.fx_triangulation_engine.pricing.triangulation;
 
 public class TriangulationEngine {
 
+    public double computeCrossRate(double price1, double price2) {
+        return price1 * price2;
+    }
 }
