983262689d0f7cb5dbc3d181ab598f2197985d52
FarhanR675 <farhanrahman675@gmail.com>
2026-04-16 12:33:42 +0100

    Implementing triangulation engine into the pricing service class

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingService.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingService.java
@@ -4,15 +4,20 @@ import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
 import com.farhan.quant.fx_triangulation_engine.domain.Price;
 import com.farhan.quant.fx_triangulation_engine.pricing.alpha.AlphaPriceGenerator;
 import com.farhan.quant.fx_triangulation_engine.pricing.spread.SpreadCalculator;
+import com.farhan.quant.fx_triangulation_engine.pricing.triangulation.TriangulationEngine;
 
 public class PricingService {
 
     private final AlphaPriceGenerator alphaPriceGenerator;
     private final SpreadCalculator spreadCalculator;
+    private final TriangulationEngine triangulationEngine;
 
-    public PricingService(AlphaPriceGenerator alphaPriceGenerator, SpreadCalculator spreadCalculator) {
+    public PricingService(AlphaPriceGenerator alphaPriceGenerator,
+                          SpreadCalculator spreadCalculator,
+                          TriangulationEngine triangulationEngine) {
         this.alphaPriceGenerator = alphaPriceGenerator;
         this.spreadCalculator = spreadCalculator;
+        this.triangulationEngine = triangulationEngine;
     }
 
     public Price getPrice(CurrencyPair currencyPair, double spread) {
