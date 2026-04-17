0a9d6f340b0e9191696776552ec151d6c1565c3f
FarhanR675 <farhanrahman675@gmail.com>
2026-04-17 15:22:09 +0100

    Implementation of getPrice mapping

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/controller/PricingController.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/controller/PricingController.java
@@ -1,7 +1,24 @@
 package com.farhan.quant.fx_triangulation_engine.controller;
 
+import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
+import com.farhan.quant.fx_triangulation_engine.domain.Price;
+import com.farhan.quant.fx_triangulation_engine.pricing.service.PricingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PricingController {
+
+    private final PricingService pricingService;
+
+    public PricingController(PricingService pricingService) {
+        this.pricingService = pricingService;
+    }
+    @GetMapping("/price")
+    public Price getPrice(@RequestParam String base,
+                          @RequestParam String quote) {
+        CurrencyPair currencyPair = new CurrencyPair(base, quote);
+        return pricingService.getPrice(currencyPair);
+    }
 }
