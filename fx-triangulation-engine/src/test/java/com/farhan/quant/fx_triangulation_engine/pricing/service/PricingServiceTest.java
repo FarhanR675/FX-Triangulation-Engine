f720aafb6ca9d13e98a5d31b7b17f4483e9f368a
FarhanR675 <farhanrahman675@gmail.com>
2026-04-17 13:00:30 +0100

    Pricing Service Test updated to try different test cases (Direct Pricing, Triangulation, Inversion, Failure Paths)

--- fx-triangulation-engine/src/test/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingServiceTest.java
+++ fx-triangulation-engine/src/test/java/com/farhan/quant/fx_triangulation_engine/pricing/service/PricingServiceTest.java
@@ -7,23 +7,59 @@ import com.farhan.quant.fx_triangulation_engine.pricing.alpha.SimpleAlphaPriceGe
 import com.farhan.quant.fx_triangulation_engine.pricing.spread.FixedSpreadCalculator;
 import com.farhan.quant.fx_triangulation_engine.pricing.spread.SpreadCalculator;
 import com.farhan.quant.fx_triangulation_engine.pricing.triangulation.TriangulationEngine;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PricingServiceTest {
 
-    @Test
-    void shouldGeneratePriceForEurJpyUsingTriangulation() {
-        // Arrange
+    private PricingService pricingService;
+
+    @BeforeEach
+    void setUp() {
         AlphaPriceGenerator alphaPriceGenerator = new SimpleAlphaPriceGenerator();
         SpreadCalculator spreadCalculator = new FixedSpreadCalculator();
         TriangulationEngine triangulationEngine = new TriangulationEngine();
 
-        PricingService pricingService = new PricingService(alphaPriceGenerator, spreadCalculator, triangulationEngine);
+        pricingService = new PricingService(alphaPriceGenerator, spreadCalculator, triangulationEngine);
+    }
 
-        //Act
-        //Price price = pricingService.getPrice(new CurrencyPair("EUR", "JPY"), 0.002);
+    @Test
+    void shouldReturnDirectPrice_EurUsd() {
+        Price price = pricingService.getPrice(new CurrencyPair("EUR", "USD"), 0.002);
+
+        assertEquals(1.10, price.getMid(), 0.0001);
+        assertEquals(1.099, price.getBid(), 0.001);
+        assertEquals(1.101, price.getAsk(), 0.001);
+    }
+
+    @Test
+    void shouldTriangulate_EurJpy() {
+        Price price = pricingService.getPrice(new CurrencyPair("EUR", "JPY"), 0.002);
+
+        assertEquals(165.0, price.getMid(), 0.0001);
+        assertEquals(164.999, price.getBid(), 0.001);
+        assertEquals(165.001, price.getAsk(), 0.001);
+    }
+
+    @Test
+    void shouldHandleInverse_JpyEur() {
+        Price price = pricingService.getPrice(new CurrencyPair("JPY", "EUR"), 0.002);
+
+        double expectedMid = 1.0 / 165.0;
+
+        assertEquals(expectedMid, price.getMid(), 0.0001);
+    }
+
+    @Test
+    void shouldThrow_WhenNoPathExists() {
+        assertThrows(RuntimeException.class, () -> pricingService.getPrice(new CurrencyPair("GBP", "USD"), 0.02));
+    }
+
+    @Test
+    void shouldGeneratePriceForEurJpyUsingTriangulation() {
+        // Arrange & Act
         Price price = pricingService.getPrice(new CurrencyPair("EUR", "JPY"), 0.002);
 
         //Assert
