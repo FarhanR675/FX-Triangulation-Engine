08d7a7903ca974af3b20617a437e560707c4dd28
FarhanR675 <farhanrahman675@gmail.com>
2026-04-17 14:42:05 +0100

    Creation of pricingConfig class and constructor

---
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/PricingConfig.java
@@ -0,0 +1,17 @@
+package com.farhan.quant.fx_triangulation_engine.config;
+
+import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PricingConfig {
+
+    private final Map<CurrencyPair, Double> spreads = new HashMap<>();
+
+    public PricingConfig() {
+        spreads.put(new CurrencyPair("EUR", "USD"), 0.001);
+        spreads.put(new CurrencyPair("USD", "JPY"), 0.002);
+        spreads.put(new CurrencyPair("EUR", "JPY"), 0.003);
+    }
+}
