0edbf1abc70617e26119101dc036dd6dbf78e74e
FarhanR675 <farhanrahman675@gmail.com>
2026-04-17 15:09:47 +0100

    inital set up of pricingController class

---
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/controller/PricingController.java
@@ -0,0 +1,7 @@
+package com.farhan.quant.fx_triangulation_engine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PricingController {
+}
