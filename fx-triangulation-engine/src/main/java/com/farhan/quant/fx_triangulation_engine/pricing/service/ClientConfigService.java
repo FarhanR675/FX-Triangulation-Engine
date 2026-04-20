6ce2a0051cf17841fe5480a48b67e667aaa87f91
FarhanR675 <farhanrahman675@gmail.com>
2026-04-20 10:16:31 +0100

    Implementation of client A and B

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/ClientConfigService.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/service/ClientConfigService.java
@@ -1,4 +1,22 @@
 package com.farhan.quant.fx_triangulation_engine.pricing.service;
 
+import com.farhan.quant.fx_triangulation_engine.config.ClientConfig;
+import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClientConfigService {
+
+    private final Map<String, ClientConfig>  clients = new HashMap<>();
+
+    public ClientConfigService() {
+        ClientConfig clientA = new ClientConfig("CLIENT_A", 0.002);
+        clientA.addPairSpread(new CurrencyPair("EUR", "USD"), 0.001);
+
+        ClientConfig clientB = new ClientConfig("CLIENT_B", 0.005);
+
+        clients.put("CLIENT_A", clientA);
+        clients.put("CLIENT_B", clientB);
+    }
 }
