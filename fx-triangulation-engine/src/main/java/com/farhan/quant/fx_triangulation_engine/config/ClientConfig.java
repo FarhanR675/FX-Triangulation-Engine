ef52c8f36c38b157e6edff8d0dd468703fcc5893
FarhanR675 <farhanrahman675@gmail.com>
2026-04-20 09:51:29 +0100

    Class fields and constructors

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/ClientConfig.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/ClientConfig.java
@@ -1,8 +1,21 @@
 package com.farhan.quant.fx_triangulation_engine.config;
 
+import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ClientConfig {
 
+    private final String clientId;
+    private final double defaultSpread;
+    private final Map<CurrencyPair, Double> painSpreads;
+
+    public ClientConfig(String clientId, double defaultSpread) {
+        this.clientId = clientId;
+        this.defaultSpread = defaultSpread;
+        this.painSpreads = new HashMap<>();
+    }
 }
