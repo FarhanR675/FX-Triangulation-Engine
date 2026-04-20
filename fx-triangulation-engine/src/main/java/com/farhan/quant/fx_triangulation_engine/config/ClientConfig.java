b2986aeff8b57fd571acf72534b2c037ec98c00e
FarhanR675 <farhanrahman675@gmail.com>
2026-04-20 10:02:35 +0100

    Methods implemented into client config class

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/ClientConfig.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/ClientConfig.java
@@ -11,11 +11,21 @@ public class ClientConfig {
 
     private final String clientId;
     private final double defaultSpread;
-    private final Map<CurrencyPair, Double> painSpreads;
+    private final Map<CurrencyPair, Double> pairSpreads;
 
     public ClientConfig(String clientId, double defaultSpread) {
         this.clientId = clientId;
         this.defaultSpread = defaultSpread;
-        this.painSpreads = new HashMap<>();
+        this.pairSpreads = new HashMap<>();
+    }
+
+    public double getSpread(CurrencyPair currencyPair) {
+        return pairSpreads.getOrDefault(currencyPair, defaultSpread);
+    }
+    public void addPairSpread(CurrencyPair currencyPair, double spread) {
+        pairSpreads.put(currencyPair, spread);
+    }
+    public String getClientId() {
+        return clientId;
     }
 }
