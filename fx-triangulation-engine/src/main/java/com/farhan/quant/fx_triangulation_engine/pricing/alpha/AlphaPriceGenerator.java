83c5cdd127b7d1e48a0d1feb087429d4572e2a35
FarhanR675 <farhanrahman675@gmail.com>
2026-04-20 13:27:41 +0100

    Added get all prices method to interface to avoid casting - cleaner code

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/alpha/AlphaPriceGenerator.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/alpha/AlphaPriceGenerator.java
@@ -2,7 +2,11 @@ package com.farhan.quant.fx_triangulation_engine.pricing.alpha;
 
 import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
 
+import java.util.Map;
+
 public interface AlphaPriceGenerator {
 
     double generateMidPrice(CurrencyPair currencyPair);
+
+    Map<CurrencyPair, Double> getAllPrices();
 }
