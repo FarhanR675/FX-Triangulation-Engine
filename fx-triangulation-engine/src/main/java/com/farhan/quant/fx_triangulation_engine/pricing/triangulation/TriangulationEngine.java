e3507fa97bcae30a0581f7fb3f8b71562b6401c8
FarhanR675 <farhanrahman675@gmail.com>
2026-04-20 12:22:33 +0100

    Triangulation Engine updated to graphs, support unlimited currencies

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/triangulation/TriangulationEngine.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/pricing/triangulation/TriangulationEngine.java
@@ -3,10 +3,7 @@ package com.farhan.quant.fx_triangulation_engine.pricing.triangulation;
 import com.farhan.quant.fx_triangulation_engine.domain.CurrencyPair;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Component
 public class TriangulationEngine {
@@ -62,36 +59,21 @@ public class TriangulationEngine {
         return null;
     }
 
-    public double computeCrossRate(CurrencyPair currencyPair1, double price1,
-                                   CurrencyPair currencyPair2, double price2,
+    public double computeCrossRate(Map<CurrencyPair, Double> prices,
                                    CurrencyPair target
-    ) { // normalising currencyPair1 so ut flows into currencyPair2
-        if (!currencyPair1.getQuote().equals(currencyPair2.getBase())) {
-            // inverting currencyPair1
-            if (currencyPair1.getBase().equals(currencyPair2.getBase())) {
-                currencyPair1 = currencyPair1.invert();
-                price1 = 1 / price1;
-            } else if (currencyPair1.getQuote().equals(currencyPair2.getQuote())) {
-                currencyPair2 = currencyPair2.invert(); // 5000
-                price2 = 1 / price2;
-            } else {
-                throw new RuntimeException("Pairs cannot be aligned");
-            }
-        }
+    ) {
+        Map<String, Map<String, Double>> graph = buildGraph(prices);
 
-        String base = currencyPair1.getBase();
-        String quote = currencyPair2.getQuote();
+        Double result = dfs(
+                target.getBase(),
+                target.getQuote(),
+                graph,
+                new HashSet<>(),
+                1.0
+        );
 
-        CurrencyPair derived = new CurrencyPair(base, quote);
-
-        double result = price1 * price2;
-
-        if (!derived.equals(target)) {
-            if (derived.invert().equals(target)) {
-                return 1 / result;
-            } else {
-                throw new RuntimeException("Target mismatch");
-            }
+        if (result == null) {
+            throw new RuntimeException("No path found for: " + target);
         }
         return result;
     }
