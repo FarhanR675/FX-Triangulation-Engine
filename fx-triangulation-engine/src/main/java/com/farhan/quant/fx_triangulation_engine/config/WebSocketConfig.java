8435db521e8121b121e3afbef5dc32412ddd6d60
FarhanR675 <farhanrahman675@gmail.com>
2026-04-21 11:32:59 +0100

    Implementation of endpoints and message broker in the Web socket config class

--- fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/WebSocketConfig.java
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/WebSocketConfig.java
@@ -1,5 +1,22 @@
 package com.farhan.quant.fx_triangulation_engine.config;
 
 
-public class WebSocketConfig {
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // This is where client subscribe
+        config.setApplicationDestinationPrefixes("/app"); // This is where clients send
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-prices")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
 }
