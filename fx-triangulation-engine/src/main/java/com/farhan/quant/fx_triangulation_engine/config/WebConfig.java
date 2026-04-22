5cef4a6a5985c08b867052281a62c696b63d3c30
FarhanR675 <farhanrahman675@gmail.com>
2026-04-22 16:52:28 +0100

    web config class created to allow cors registry

---
+++ fx-triangulation-engine/src/main/java/com/farhan/quant/fx_triangulation_engine/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.farhan.quant.fx_triangulation_engine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
